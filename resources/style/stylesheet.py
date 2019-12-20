ad72a743d031046cd74ffaf42078d1856a3146eb
Skengrek <armand.masse@cpe.fr>
2019-12-20 16:56:10 +0100

    Added a debug stylesheet

---
+++ resources/style/stylesheet.py
@@ -0,0 +1,30 @@
+"""
+
+"""
+
+MISC_BUTTON = """
+QPushButton{
+    background-color: grey;
+    border: black;
+}
+
+"""
+
+DEBUG = """
+QLabel{
+    background-color: yellow;
+}
+
+QComboBox{
+    background-color: red;
+}
+
+QLineEdit{
+    background-color: purple;
+}
+
+QSlider{
+    background-color: pink;
+}
+
+"""
