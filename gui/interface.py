dd23615a1cee21a967e5250ba9a4ada28fdb5f10
Skengrek <armand.masse@cpe.fr>
2019-12-29 00:32:15 +0100

    Change the basic definition to use New misc widgets

--- gui/interface.py
+++ gui/interface.py
@@ -87,6 +87,7 @@ class EditWidget(QtWidgets.QWidget):
         self.ability = None
         self.img = None
         self.slider_value = 0
+        gen_list = ['Black & White']
 
         self.setMinimumWidth(200)
         # ?####################################################################
@@ -95,138 +96,96 @@ class EditWidget(QtWidgets.QWidget):
 
         # TODO add basic option seletcted for QComboBox
 
-        self.name_label = QtWidgets.QLabel('Name')
-        self.name_edit = QtWidgets.QLineEdit()
-        self.name_edit.textChanged.connect(self.generate_dict)
+        self.name = misc.LabelEdit('Name')
+        self.name.edit_sig.connect(self.generate_dict)
 
-        self.stage_label = QtWidgets.QLabel('Stage')
-        self.stage_edit = QtWidgets.QComboBox()
-        # TODO add other stage
-        self.stage_edit.addItems(['Basic', 'Stage1', 'Stage2'])
-        self.stage_edit.setCurrentIndex(0)
-        self.stage_edit.currentTextChanged.connect(self.generate_dict)
+        tmp_list = ['Basic', 'Stage1', 'Stage2']
+        self.stage = misc.LabelComboBox('Stage', tmp_list)
+        self.stage.edit_sig.connect(self.generate_dict)
 
-        self.type_choice = QtWidgets.QComboBox()
-        self.type_choice.addItems(type_list)
-        self.type_choice.currentTextChanged.connect(self.change_background)
-        self.type_choice.setCurrentIndex(0)
-
-        self.background_choice = QtWidgets.QComboBox()
-        self.background_choice.currentTextChanged.connect(self.generate_dict)
+        self.type = misc.LabelComboBox('Type', type_list, 'colorless')
+        self.type.combo[0].currentTextChanged.connect(self.change_background)
+        self.type.combo[1].currentTextChanged.connect(self.generate_dict)
 
         # self.change_background('Basic')
 
-        self.hp_label = QtWidgets.QLabel('Health')
-        self.hp_edit = QtWidgets.QLineEdit()
-        self.hp_edit.textChanged.connect(self.generate_dict)
+        self.hp = misc.LabelEdit('Health')
+        self.hp.edit_sig.connect(self.generate_dict)
 
-        self.size_label = QtWidgets.QLabel('Size')
-        self.size_edit = QtWidgets.QLineEdit()
-        self.size_edit.textChanged.connect(self.generate_dict)
+        self.size = misc.LabelEdit('Size')
+        self.size.edit_sig.connect(self.generate_dict)
 
-        self.weight_label = QtWidgets.QLabel('Weight')
-        self.weight_edit = QtWidgets.QLineEdit()
-        self.weight_edit.textChanged.connect(self.generate_dict)
+        self.weight = misc.LabelEdit('Weight')
+        self.weight.edit_sig.connect(self.generate_dict)
 
-        self.weak_label = QtWidgets.QLabel('Weakness')
-        self.weak_choice = QtWidgets.QComboBox()
-        self.weak_choice.addItems(type_list)
-        self.weak_choice.currentTextChanged.connect(self.generate_dict)
+        self.weak = misc.LabelComboBox('Weakness', type_list)
+        self.weak.edit_sig.connect(self.generate_dict)
 
-        self.resist_label = QtWidgets.QLabel('Resistance')
-        self.resist_choice = QtWidgets.QComboBox()
-        self.resist_choice.addItems(type_list)
-        self.resist_choice.currentTextChanged.connect(self.generate_dict)
-        self.resist_value = QtWidgets.QComboBox()
-        self.resist_value.addItems(['-10', '-20', '-30'])
-        self.resist_value.currentTextChanged.connect(self.generate_dict)
+        tmp_list = ['-10', '-20', '-30']
+        self.resist = misc.LabelComboBox('Resistance', type_list, tmp_list)
+        self.resist.edit_sig.connect(self.generate_dict)
 
-        self.retreat_label = QtWidgets.QLabel('Retreat')
-        self.retreat_value = QtWidgets.QComboBox()
-        self.retreat_value.addItems(['0', '1', '2', '3', '4'])
-        self.retreat_value.currentTextChanged.connect(self.generate_dict)
+        tmp_list = ['0', '1', '2', '3', '4']
+        self.retreat = misc.LabelComboBox('Retreat', tmp_list)
+        self.retreat.edit_sig.connect(self.generate_dict)
 
-        self.desc_label = QtWidgets.QLabel('Description')
-        self.desc_edit = QtWidgets.QLineEdit()
-        self.desc_edit.textChanged.connect(self.generate_dict)
+        self.desc = misc.LabelEdit('Description')
+        self.desc.edit_sig.connect(self.generate_dict)
 
-        self.setNb_label = QtWidgets.QLabel('Set numbers')
-        self.this_number = QtWidgets.QLineEdit()
-        self.this_number.textChanged.connect(self.generate_dict)
-        self.div_label = QtWidgets.QLabel('/')
-        self.max_edit = QtWidgets.QLineEdit()
-        self.max_edit.textChanged.connect(self.generate_dict)
+        self.setNb = misc.LabelEdit('Set number', '/')
+        self.setNb.edit_sig.connect(self.generate_dict)
 
-        self.illustrator_label = QtWidgets.QLabel('Illustrator')
-        self.illustrator_edit = QtWidgets.QLineEdit()
-        self.illustrator_edit.textChanged.connect(self.generate_dict)
+        self.illustrator = misc.LabelEdit('Illustrator')
+        self.illustrator.edit_sig.connect(self.generate_dict)
 
-        self.gen_label = QtWidgets.QLabel('Generation')
-        self.gen_value = QtWidgets.QComboBox()
-        self.gen_value.addItems(['Black & White'])
-        self.gen_value.currentTextChanged.connect(self.generate_dict)
+        self.gen = misc.LabelComboBox('Generation', gen_list)
+        self.gen.edit_sig.connect(self.generate_dict)
 
-        self.copyright_label = QtWidgets.QLabel('copyright')
-        self.copyright_edit = QtWidgets.QLineEdit()
-        self.copyright_edit.textChanged.connect(self.generate_dict)
+        self.copyright = misc.LabelEdit('Copyright')
+        self.copyright.edit_sig.connect(self.generate_dict)
 
         # ?####################################################################
         # ? Layout
         # ?####################################################################
 
-        layout = QtWidgets.QGridLayout()
+        layout = QtWidgets.QVBoxLayout()
 
-        layout.addWidget(self.gen_label, 0, 0, 1, 1)
-        layout.addWidget(self.gen_value, 0, 1, 1, -1)
+        layout.addWidget(self.gen)
 
-        layout.addWidget(self.name_label, 1, 0, 1, 1)
-        layout.addWidget(self.name_edit, 1, 1, 1, -1)
+        layout.addWidget(self.name)
 
-        layout.addWidget(self.stage_label, 2, 0, 1, 1)
-        layout.addWidget(self.stage_edit, 2, 1, 1, -1)
+        layout.addWidget(self.stage)
 
-        layout.addWidget(self.type_choice, 3, 0, 1, 1)
-        layout.addWidget(self.background_choice, 3, 1, 1, -1)
+        layout.addWidget(self.type)
 
-        layout.addWidget(self.hp_label, 4, 0, 1, 1)
-        layout.addWidget(self.hp_edit, 4, 1, 1, -1)
+        layout.addWidget(self.hp)
 
-        layout.addWidget(self.size_label, 5, 0, 1, 1)
-        layout.addWidget(self.size_edit, 5, 1, 1, -1)
+        layout.addWidget(self.size)
 
-        layout.addWidget(self.weight_label, 6, 0, 1, 1)
-        layout.addWidget(self.weight_edit, 6, 1, 1, -1)
+        layout.addWidget(self.weight)
 
-        layout.addWidget(self.weak_label, 9, 0, 1, 1)
-        layout.addWidget(self.weak_choice, 9, 1, 1, -1)
+        layout.addWidget(self.weak)
 
-        layout.addWidget(self.resist_label, 10, 0, 1, 1)
-        layout.addWidget(self.resist_choice, 10, 1, 1, 1)
-        layout.addWidget(self.resist_value, 10, 2, 1, -1)
+        layout.addWidget(self.resist)
 
-        layout.addWidget(self.retreat_label, 11, 0, 1, 1)
-        layout.addWidget(self.retreat_value, 11, 1, 1, -1)
+        layout.addWidget(self.retreat)
 
-        layout.addWidget(self.desc_label, 12, 0, 1, 1)
-        layout.addWidget(self.desc_edit, 12, 1, 1, -1)
+        layout.addWidget(self.desc)
 
-        layout.addWidget(self.illustrator_label, 17, 0, 1, 1)
-        layout.addWidget(self.illustrator_edit, 17, 1, 1, -1)
+        layout.addWidget(self.illustrator)
 
-        layout.addWidget(self.setNb_label, 18, 0, 1, 1)
-        layout.addWidget(self.this_number, 18, 1, 1, 1)
-        layout.addWidget(self.div_label, 18, 2, 1, 1)
-        layout.addWidget(self.max_edit, 18, 3, 1, 1)
+        layout.addWidget(self.setNb)
 
-        layout.addWidget(self.copyright_label, 19, 0, 1, 1)
-        layout.addWidget(self.copyright_edit, 19, 1, 1, -1)
+        layout.addWidget(self.copyright)
+
+        layout.addStretch(1)
 
         self.setLayout(layout)
 
     def change_background(self, e):
         """Get all the background available for a specific type"""
         _type = e
-        self.background_choice.clear()
+        self.type.combo[1].clear()
 
         if _type is not None:
             file_list = listdir('resources' + sep + 'background' + sep)
@@ -237,33 +196,33 @@ class EditWidget(QtWidgets.QWidget):
             if _type.lower() == 'basic':
                 list_available_background.append('colorless')
 
-            self.background_choice.addItems(list_available_background)
+            self.type.combo[1].addItems(list_available_background)
             self.update()
             self.generate_dict()
 
     def generate_dict(self):
         _dict = {
-            'name': self.name_edit.text(),
-            'stage': self.stage_edit.currentText().lower(),
-            'type': self.type_choice.currentText().lower(),
-            'background': self.background_choice.currentText(),
-            'health': self.hp_edit.text(),
+            'name': self.name.edit[0].text(),
+            'stage': self.stage.combo[0].currentText().lower(),
+            'type': self.type.combo[0].currentText().lower(),
+            'background': self.type.combo[1].currentText(),
+            'health': self.hp.edit[0].text(),
             'image': self.img,
-            'height': self.size_edit.text().lower(),
-            'weight': self.weight_edit.text().lower(),
+            'height': self.size.edit[0].text().lower(),
+            'weight': self.weight.edit[0].text().lower(),
             'ability': self.ability,
             'attack': self.attacks,
             'space': self.slider_value,
-            'weakness': self.weak_choice.currentText().lower(),
-            'resistance': [self.resist_choice.currentText().lower(),
-                           self.resist_value.currentText().lower()],
-            'retreat': int(self.retreat_value.currentText().lower()),
-            'description': self.desc_edit.text(),
-            'set_number': self.this_number.text(),
-            'set_maximum': self.max_edit.text(),
-            'illustrator': self.illustrator_edit.text(),
-            'generation': self.gen_value.currentText(),
-            'copyright': self.copyright_edit.text(),
+            'weakness': self.weak.combo[0].currentText().lower(),
+            'resistance': [self.resist.combo[0].currentText().lower(),
+                           self.resist.combo[1].currentText().lower()],
+            'retreat': int(self.retreat.combo[0].currentText().lower()),
+            'description': self.desc.edit[0].text(),
+            'set_number': self.setNb.edit[0].text(),
+            'set_maximum': self.setNb.edit[1].text(),
+            'illustrator': self.illustrator.edit[0].text(),
+            'generation': self.gen.combo[0].currentText(),
+            'copyright': self.copyright.edit[0].text(),
         }
 
         self.changed_dict_sig.emit(_dict)
