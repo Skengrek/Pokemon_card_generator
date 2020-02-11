eb97894c0703caa62c0a766ea6bc6b78bb9d23f3
Skengrek <armand.masse@cpe.fr>
2020-02-11 09:04:21 +0100

    WIP

--- src/BW.py
+++ src/BW.py
@@ -10,12 +10,12 @@ import os
 
 # ! PIL imports
 from PIL import Image, ImageQt
-from PIL.ImageFont import truetype
 from PIL.ImageDraw import Draw
 from PIL.Image import alpha_composite
 
-from .. import resources
-from .image import Type
+import resources
+
+from .image import Font, Type
 from .logginginit import get_logger
 
 # * Logger
@@ -42,7 +42,9 @@ class BW(object):
         # * ##################################################################
 
         self.font = Font()
+        logger.debug(self.font)
         self.type = Type(_type)
+        logger.debug(self.type)
 
         # * Initialise basic part
         # * ##################################################################
@@ -51,13 +53,15 @@ class BW(object):
         self.text = None
 
         bc_path = os.path.join(resources.path(), 'BW', 'blank.png')
-        self.blank_card = Image.open(bc_path)
+        blank_card = Image.open(bc_path)
 
-        self.x_max, self.y_max = self.blank_card.size
+        self.x_max, self.y_max = blank_card.size
 
-    def initialise_blank(self):
+        self.initialise_blank(blank_card)
+
+    def initialise_blank(self, img_blank):
         """
-        Create the base text for the card
+        Create the base text for the card with the blank card
         Returns:
         """
         pass
@@ -74,60 +78,12 @@ class BW(object):
         self.type = Type(_type)
 
 
-class Font(object):
-    """
-    This class contains all font used in the images ready to be used by PIL
-    """
-
-    def __init__(self):
-        """
-        Initialise the fonts for the black and white generation card
-        """
-
-        font = os.path.join(resources.path(), 'fonts')
-        font = os.path.normpath(font)
-
-        # * Font Path
-        # * ##################################################################
-
-        # ? gill based font
-        g_std = os.path.join(font, 'GillSansStd.otf')
-        g_bold = os.path.join(font, 'GillSansStd-Bold.otf')
-        g_bold_cond = os.path.join(font, 'GillSansStd-BoldCondensed.otf')
-
-        # ? futura based font
-        f_med = os.path.join(font, 'FuturaStd-Medium.otf')
-        f_bold = os.path.join(font, 'FuturaStd-Bold.otf')
-        f_cond_bold = os.path.join(font, 'FuturaStd-CondensedBold.otf')
-        f_bold_cond_obl = os.path.join(font, 'FuturaStd-CondensedBoldObl.otf')
-
-        # ? SanvitoPro based font
-        s_bold = os.path.join(font, 'SanvitoPro-Bold.otf')
-
-        # * Font definition for PIL
-        # * ##################################################################
-
-        self.name = truetype(g_bold, 23)
-        self.hp_str = truetype(g_bold, 11)
-        self.hp_nbr = truetype(f_cond_bold, 11)
-        self.info = truetype(g_std, 9)
-        self.ability_name = truetype(g_bold_cond, 9)
-        self.ability_text = truetype(g_std, 16)
-        self.damage = truetype(f_bold, 16)
-        self.weakness = truetype(g_bold, 15)
-        self.illustrator = truetype(f_bold_cond_obl, 11)
-        self.copyright = truetype(f_med, 7)
-        self.description = truetype(s_bold, 13)
-        self.misc_text = truetype(g_bold, 10)
-
-
 # ! Main and tester
 # ! ##########################################################################
 
 
 def main():
     img = BW('dragon')
-
 
 if __name__ == "__main__":
     main()
--- src/image.py
+++ src/image.py
@@ -5,6 +5,9 @@
 import os
 from .logginginit import get_logger
 
+from PIL import Image
+from PIL.ImageFont import truetype
+
 from src.BW_old import bw
 
 import resources
@@ -49,8 +52,10 @@ class Type(object):
         # ? ##################################################################
 
         icons_path = os.path.join(resources.path(), 'icons')
-        self.icon = os.path.join(icons_path, self.type + '.png')
-        self.icon_small = os.path.join(icons_path, self.type+'_small.png')
+        icon_path = os.path.join(icons_path, self.type + '.png')
+        self.icon = Image.open(icon_path)
+        icon_path_s = os.path.join(icons_path, self.type + '_small.png')
+        self.icon_small = Image.open(icon_path_s)
 
         # ? Background
         # ? ##################################################################
@@ -101,6 +106,21 @@ class Type(object):
 
         self.set_color()
 
+    # * repr
+    # * ######################################################################
+
+    def __repr__(self):
+        _str = '\nType with the following parameters:\n'
+        _str += '\tIcons:\n'
+        _str += '\t\tinfo:' + str(self.icon.info) + '\n'
+        _str += '\t\tsize:' + str(self.icon.size) + '\n'
+        _str += '\tIcons_small:\n'
+        _str += '\t\tinfo:' + str(self.icon_small.info) + '\n'
+        _str += '\t\tsize:' + str(self.icon_small.size) + '\n'
+        _str += '\tAvailable background:\n'
+        _str += '\t\t' + str(self.available_background) + '\n'
+        return str(_str)
+
 
 class Color(object):
     """
@@ -122,10 +142,65 @@ class Color(object):
         self.text = text
         self.border = border
         self.ability = ability
-        logger.info('The coloe has been created with the following parameter'
+        logger.info('The color has been created with the following parameter'
                     '{text}, {border}, {ability}'
                     .format(text=self.text, border=self.border,
                             ability=self.ability))
+
+
+class Font(object):
+    """
+    This class contains all font used in the images ready to be used by PIL
+    """
+
+    def __init__(self):
+        """
+        Initialise the fonts for the black and white generation card
+        """
+
+        font = os.path.join(resources.path(), 'fonts')
+        font = os.path.normpath(font)
+
+        # * Font Path
+        # * ##################################################################
+
+        # ? gill based font
+        g_std = os.path.join(font, 'GillSansStd.otf')
+        g_bold = os.path.join(font, 'GillSansStd-Bold.otf')
+        g_bold_cond = os.path.join(font, 'GillSansStd-BoldCondensed.otf')
+
+        # ? futura based font
+        f_med = os.path.join(font, 'FuturaStd-Medium.otf')
+        f_bold = os.path.join(font, 'FuturaStd-Bold.otf')
+        f_cond_bold = os.path.join(font, 'FuturaStd-CondensedBold.otf')
+        f_bold_cond_obl = os.path.join(font, 'FuturaStd-CondensedBoldObl.otf')
+
+        # ? SanvitoPro based font
+        s_bold = os.path.join(font, 'SanvitoPro-Bold.otf')
+
+        # * Font definition for PIL
+        # * ##################################################################
+
+        self.name = truetype(g_bold, 23)
+        self.hp_str = truetype(g_bold, 11)
+        self.hp_nbr = truetype(f_cond_bold, 11)
+        self.info = truetype(g_std, 9)
+        self.ability_name = truetype(g_bold_cond, 9)
+        self.ability_text = truetype(g_std, 16)
+        self.damage = truetype(f_bold, 16)
+        self.weakness = truetype(g_bold, 15)
+        self.illustrator = truetype(f_bold_cond_obl, 11)
+        self.copyright = truetype(f_med, 7)
+        self.description = truetype(s_bold, 13)
+        self.misc_text = truetype(g_bold, 10)
+
+    def __repr__(self):
+        # ! Override print function for type
+        _str = '\nthis Font is initialised with :\n'
+        _str += str(self.__dict__.keys())
+        return _str
+
+
 
 
 # ! Main and tester
